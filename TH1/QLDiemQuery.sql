4e87e6a4b4ad78ab48e44464d45a1df84288a69d
TaiTitans <taititansofficial@gmail.com>
2023-09-29 00:07:16 +0700

    update

--- TH1/QLDiemQuery.sql
+++ TH1/QLDiemQuery.sql
@@ -76,3 +76,72 @@ from sinhvien sv
 join
 ketqua kq on sv.mssv = kq.mssv
 order by sv.maKhoa ASC, sv.hoTen ASC;
+
+# Cau 11
+update ketqua kq
+join sinhvien sv on kq.mssv = sv.mssv
+set kq.diem = kq.diem + 1
+where sv.maKhoa <> 'CNTT&TT' and kq.maHP = 'CT101';
+
+# Cau 12
+
+select sv.mssv, sv.hoTen
+from sinhvien sv
+join ketqua kq on sv.mssv = kq.mssv
+where kq.maHP = 'CT101' and sv.mssv in (
+select mssv from ketqua where maHP = 'CT176');
+
+# Cau 13
+select sv.mssv, sv.hoten
+from sinhvien sv
+join ketqua kq on sv.mssv = kq.mssv
+join hocphan hp on kq.maHP = hp.maHP
+where hp.tenHP <> 'Anh văn căn bản 1';
+
+# Cau 14
+select sv.hoten, sv.noisinh, sv.maKhoa
+from sinhvien sv
+where sv.ngaySinh = (select max(ngaySinh) from sinhvien);
+
+# Cau 15
+
+select k.maKhoa, k.tenKhoa, sum(case when gioiTinh = 'M' then 1 else 0 end) as 'Số Nam',
+sum(case when gioiTinh = 'F' then 1 else 0 end) as 'Số nữ'
+from sinhvien sv
+join khoa k on sv.maKhoa = k.maKhoa
+group by maKhoa, tenKhoa;
+
+# Cau 16
+select sv.mssv, hp.maHP, sv.hoten
+from sinhvien sv
+join ketqua kq on sv.mssv = kq.mssv
+join hocphan hp on kq.maHP = hp.maHP
+where kq.diem < 5;
+
+# Cau 17
+alter table sinhvien
+modify column mssv char(8) check (mssv REGEXP '^[A-Z][0-9]{8}$');
+
+# Cau 18
+ALTER TABLE sinhvien
+modify column hoten varchar(45)
+CHECK (hoten REGEXP '^[a-zA-Z ]+$');
+
+# Cau 19
+ALTER TABLE ketqua
+modify column diem float
+CHECK(diem between 0 and 10);
+
+# Cau 20
+ALTER TABLE sinhvien
+modify column gioitinh char(1)
+CHECK(gioitinh in('F','M'));
+
+
+SELECT hoten
+FROM sinhvien
+WHERE hoten NOT REGEXP '^[a-zA-Z ]+$';
+
+SELECT mssv
+FROM sinhvien
+WHERE mssv NOT REGEXP '^[A-Z][0-9]{8}$';
